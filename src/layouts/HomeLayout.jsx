acea361c7062ef96247c7cf385f39ae8a8c088b3
indroji-mondal <indro.cse.bu@gmail.com>
2024-11-14 13:06:13 +0600

    Added Categories

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import LatestNews from '../components/LatestNews';
 import Navbar from '../components/Navbar';
+import LeftNavbar from '../components/layoutComponent/LeftNavbar';
 
 
 
@@ -25,7 +26,7 @@ const HomeLayout = () => {
 
               </nav>
               <main className='w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3' >
-                <aside className='col-span-3'>Left Navbar</aside>
+                <aside className='col-span-3'> <LeftNavbar></LeftNavbar> </aside>
                 <section className='col-span-6'>Main content</section>
                 <aside className='col-span-3'>Right Navbar</aside>
               </main>
